92b82aee85db65d3da1632686ef87b5f21c82bd5
RaphaelRevivor <marvelous.islander@gmail.com>
2025-11-07 14:18:37 +0100

    Rearrange ui components

--- coding/raspberry/pyside_example/pyqt_example.py
+++ coding/raspberry/pyside_example/pyqt_example.py
@@ -1,18 +1,24 @@
 from PySide6.QtWidgets import QApplication, QWidget, QGridLayout, QDial, QSlider, QPushButton
+from PySide6.QtCore import Qt
 
 app = QApplication([])
 
 win = QWidget()
 layout = QGridLayout(win)
 
-dial = QDial()
-slider = QSlider()
-button = QPushButton("OK")
 
-layout.addWidget(dial, 0, 0)
-layout.addWidget(slider, 0, 0)
-# row=1, col=0, span 2 columns
-layout.addWidget(button, 1, 0, 1, 2)
+for i in range(2):
+  knob = QDial()
+
+  layout.addWidget(knob, 2 * i + 1, 2, 1, 2)
+
+for i in range(2):
+  slider = QSlider(Qt.Horizontal)
+  layout.addWidget(slider, 2 * i + 1, 0, 2, 2)
+
+for i in range(4):
+  button = QPushButton("OK")
+  layout.addWidget(button, 4, i, 1, 1)
 
 win.show()
 app.exec()
