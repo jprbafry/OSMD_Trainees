63072f56fcd8df5d8e665c9817a9325d27b903aa
Patricia Baladjay <patriciaanna.baladjay@afry.com>
2025-11-07 09:18:43 +0100

    Modified knob class according to the comment from PR

--- coding/raspberry/dash_pygame/GUI/knob.py
+++ coding/raspberry/dash_pygame/GUI/knob.py
@@ -5,7 +5,6 @@ import random
 import time
 
 
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..')))
 from dash_pygame.GUI import widget
 
 #Knob class
@@ -17,8 +16,7 @@ class Knob(widget.Widget):
         self.cx, self.cy = cx, cy
         self.radius = radius
         self.angle = math.radians(min_val)
-        self.old_cur_val = max_val / 2
-        self.new_cur_val = max_val / 2
+        self.cur_val = max_val / 2
         self.dragging = False
         self.font = font
         self.auto = auto
@@ -27,12 +25,12 @@ class Knob(widget.Widget):
             threading.Thread(target=self._generate_data, daemon=True).start()
 
     def update_cur_val(self,val):
-        self.new_cur_val = max(self.min_val, min(self.max_val, val))
+        self.cur_val = max(self.min_val, min(self.max_val, val))
 
     def draw(self, surface):
         if not self.visible:
             return
-        # # Draw knob circle
+        # Draw knob circle
         pygame.draw.circle(surface, (200, 200, 200), (self.cx, self.cy), self.radius)
         pygame.draw.circle(surface, (100, 100, 100), (self.cx, self.cy), self.radius, 5)
 
@@ -40,17 +38,7 @@ class Knob(widget.Widget):
         angle = math.radians(self.new_cur_val)
         end_x = self.cx + self.radius * 0.8 * math.cos(angle - math.pi / 2)
         end_y = self.cy + self.radius * 0.8 * math.sin(angle - math.pi / 2)
-        pygame.draw.line(surface, (0, 0, 0), (self.cx, self.cy), (end_x, end_y), 5)
-
-        # Draw home indicator
-        home_angle = -math.pi / 2  # top
-        home_x = self.cx + self.radius * 0.9 * math.cos(home_angle)
-        home_y = self.cy + self.radius * 0.9 * math.sin(home_angle)
-        pygame.draw.circle(surface, (255, 0, 0), (int(home_x), int(home_y)), 6)
-
-        # # Draw value text
-        # value_surf = self.font.render(f"{int(self.new_cur_val)}", True, (255, 255, 255))
-        # surface.blit(value_surf, (self.cx - value_surf.get_width() // 2, self.cy + self.radius + 10))
+        pygame.draw.line(surface, (0,255, 0), (self.cx, self.cy), (end_x, end_y), 5)
 
     def _generate_data(self):
         t = 0
